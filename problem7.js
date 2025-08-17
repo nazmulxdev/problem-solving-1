88430cbce517f6d534a2bbf0e8a836d69d8f3d91
Md Nazmul Hossen <nazmulhossen.hstu.21@gmail.com>
2025-08-18 00:14:02 +0600

    adde problem 7 - find even numbers in an array

---
+++ problem7.js
@@ -0,0 +1,17 @@
+// problem 7: find even number in an array
+
+function findEven(numbersArray) {
+  try {
+    let evenArray = [];
+    for (const number of numbersArray) {
+      if (number % 2 === 0) {
+        evenArray.push(number);
+      }
+    }
+    return evenArray;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
