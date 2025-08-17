2bdd37c26235525c6ec0d00e957377b496108563
Md Nazmul Hossen <nazmulhossen.hstu.21@gmail.com>
2025-08-17 23:50:14 +0600

    added problem 5 - remove duplicates from an array

---
+++ problem5.js
@@ -0,0 +1,17 @@
+// problem 5: Remove Duplicates from an array
+
+function removeDuplicates(array) {
+  try {
+    let finalArray = [];
+    for (const num of array) {
+      if (!finalArray.includes(num)) {
+        finalArray.push(num);
+      }
+    }
+    return finalArray;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
